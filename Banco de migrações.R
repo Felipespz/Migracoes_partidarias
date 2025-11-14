702ef51398207efbcddacfae3759bb1fc5c600fc
Felipespz <94149558+Felipespz@users.noreply.github.com>
2025-11-14 13:35:44 -0300

    Update Banco de migrações.R

--- Banco de migrações.R
+++ Banco de migrações.R
@@ -4,7 +4,7 @@ library(tidyverse)
 library(patchwork)
 library(officer)     # Para limpeza de texto na junção das variáveis
 library(stringi)     # Para limpeza de texto na junção das variáveis
-library(lmtest)     # Para teste de razão de verossimilhança
+library(lmtest)      # Para aplicação do General Linear Model "glm()"
 
 #Puxando e transformando os dados
   banco_de_migrações_cap <- read_xlsx("C:/Users/felip/Downloads/BD_Sainz&Codato_ClassePolíticadoBrasil_INCT_ReDem_2025.xlsx")
